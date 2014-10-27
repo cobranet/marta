6fdcedc2fb68f71f7486d80bd1439e386fa23081
Bratislav Petrovic <bratislav.petrovic@gmail.com>
2014-10-27 10:18:35 +0100

    nelogovani

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,10 +1,13 @@
 class WelcomeController < ApplicationController
   def index
+    @heroji = User.heroji
   end
+
   def pitanje
     br = params[:pitanje].to_i
     render json: { pitanje: Pitanje.where(broj: br).first.pitanje }
   end
+
   def odgovor
     pitanje = params[:pitanje]
     odgovor = params[:odgovor]
@@ -15,7 +18,8 @@ class WelcomeController < ApplicationController
       rez = 'ok'
     end
     Aktivnost.zabelezi(current_user.id,pitanje,odgovor,rez)
+    tacno = current_user.tacno(pitanje)
     poruka = current_user.poruka
-    render json: {od: odgovor, pitanje: pitanje , rez: rez , poruka: poruka }
+    render json: {od: odgovor, pitanje: pitanje , rez: rez , poruka: poruka,tacno: tacno }
   end 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 class User < ActiveRecord::Base
+  # This is from omniauth gem
   def self.from_omniauth(auth)
     where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
       user.provider = auth.provider
@@ -12,19 +13,61 @@ class User < ActiveRecord::Base
       user.save!
     end
   end
+  # Who ask question .. must be moved to database
+  def cije(broj) 
+    if  broj == 1 
+       cije = " Martino " 
+    else
+      cije = "Kjarino" 
+    end  
+  end
+  # format date in Serbian ?
+  def datum(d)
+    d.strftime("%d/%m/%Y")
+  end
+
+  # WHo is solve problem
+  def self.heroji
+    a = Aktivnost.select("user_id as user_id,max(kad) as kad").where(rez: 'ok').group("user_id").order('kad').limit(10)
+    puts a
+    hero = [] 
+    a.reverse.each do |a|
+      user = User.find(a.user_id)
+      hero << { image: user.image, datum: a.kad } 
+    end 
+    hero
+  end
+
+  #Message for solvers
+  def tacno(pitanje)
+    t = Aktivnost.where(user_id: self.id, pitanje: pitanje,rez: 'ok').first
+    a = nil 
+    if t != nil
+      a= "Vi ste stvarno pametni i na #{cije(pitanje)} ste odgovorili #{datum(t.kad)}.."
+    end
+  end
+  
+  # poruka za usera
   def poruka
     last = Aktivnost.where(user_id: self.id).order(kad: :desc).first
     if last == nil 
       poruka = "Zdravo #{self.name}... Prvi put ste ovde da vidimo šta znate!"
     else
-      koliko =  Aktivnost.where(user_id: 1).count
-      if last.pitanje == 1 
-        cije = " Martino " 
-      else
-        cije = "Kjarino" 
-      end  
-      poruka = "Zdravo #{self.name}. Zadnji pokušaj je bio #{last.kad.strftime("%d/%m/%Y")} ...na #{cije} pitanje  rekli ste #{last.odgovor}...Probali ste #{koliko} puta. "
+      koliko =  Aktivnost.where(user_id: self.id ).count
+      cijep = cije(last.pitanje)
+      poruka = "Zdravo #{self.name}. Zadnji pokušaj je bio #{datum(last.kad)} ...na #{cijep} pitanje  rekli ste #{last.odgovor}...Probali ste #{koliko} puta. "
     end
     poruka 
   end  
+  # poruka za nelogovane 
+  def self.nelogovan 
+    odgovora = 0
+    usera = 0
+    koliko = Aktivnost.select("user_id as user_id,count(kad) as koliko").group("user_id")
+    koliko.each do |user|          
+      usera = usera + 1
+      odgovora = odgovora + user[:koliko]
+    end  
+    n = "Do sada je pokušalo #{usera} pametnjeakovića i probalo #{odgovora} puta"
+  end
 end
